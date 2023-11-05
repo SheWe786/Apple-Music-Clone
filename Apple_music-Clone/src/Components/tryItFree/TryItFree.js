b22239df9dbbefeaef47d57dd4ca77020813e2c5
Shivani Tiwari <116451611+SheWe786@users.noreply.github.com>
2023-11-05 16:24:18 +0530

    Apple-Music-Clone

---
+++ Apple_music-Clone/src/Components/tryItFree/TryItFree.js
@@ -0,0 +1,16 @@
+import React from "react";
+import './TryItFree.css'
+const TryItFree = () => {
+  return (
+    <div className="coming-soon-container">
+      <h1>Coming Soon</h1>
+      <p>We are working on something amazing. Stay tuned!</p>
+    </div>
+  );
+};
+
+<<<<<<< HEAD
+export default TryItFree;
+=======
+export default TryItFree;
+>>>>>>> 18daaab96b1042b120486a1add7eefa3125dc863
